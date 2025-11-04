60cc498303305cf676dd6c07fff5154977a32ffd
MarioSyaif <mariosyaif@gmail.com>
2025-11-04 09:21:03 +0700

    uts sisi klien

---
+++ src/Exercise.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PostCard from "./PostCard";
+import postsData from "./postsData";
+
+function Exercise() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        User Posts
+      </h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+        {postsData.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            userId={post.userId}
+            title={post.title}
+            body={post.body}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Exercise;
